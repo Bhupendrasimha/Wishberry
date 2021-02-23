c66582527be23fe5ed4b171ee79eebc86e7554ee
simha <bhupendrasimha@gmail.com>
2021-02-23 22:30:56 +0530

    Wishberry

---
+++ wishberry/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import Work from "./Components/routes/route";
+import Login from "./Components/pages/Loginpage/login"
+import Footer from "./Components/pages/Footer/footer";
+function App() {
+  return (
+    <div className="App">
+      <Work />
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
